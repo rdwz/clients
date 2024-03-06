d6fcfaf6804ed492f056defe0f5a92ba309e1ad2
Shane Melton <smelton@bitwarden.com>
2024-03-06 12:18:31 -0800

    [AC-1707] Add feature flag

--- libs/common/src/enums/feature-flag.enum.ts
+++ libs/common/src/enums/feature-flag.enum.ts
@@ -9,6 +9,7 @@ export enum FeatureFlag {
   FlexibleCollectionsMigration = "flexible-collections-migration",
   AC1607_PresentUserOffboardingSurvey = "AC-1607_present-user-offboarding-survey",
   ShowPaymentMethodWarningBanners = "show-payment-method-warning-banners",
+  RestrictProviderAccess = "restrict-provider-access",
 }
 
 // Replace this with a type safe lookup of the feature flag values in PM-2282
