629e0ed2d9cc1b2ecd58a52e56ce2cebcae67802
Thomas Rittson <trittson@bitwarden.com>
2024-03-15 14:35:58 +1000

    Remove outdated comment

--- apps/web/src/app/admin-console/organizations/members/components/member-dialog/member-dialog.component.ts
+++ apps/web/src/app/admin-console/organizations/members/components/member-dialog/member-dialog.component.ts
@@ -315,7 +315,6 @@ export class MemberDialogComponent implements OnDestroy {
     );
 
     // Set current collections and groups the user has access to
-    // Exclude readonly items, these only represent editable form values
     const accessSelections = mapToAccessSelections(userDetails);
     const groupAccessSelections = mapToGroupAccessSelections(userDetails.groups);
 
