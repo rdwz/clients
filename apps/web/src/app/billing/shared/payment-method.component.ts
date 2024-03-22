e59773e8548699a5d2803b1e9dc4062cb5d8d286
KiruthigaManivannan <kiruthiga.manivannan@kovanlabs.com>
2024-03-22 19:37:01 +0530

    PM-5022 Addressed review comments

--- apps/web/src/app/billing/shared/payment-method.component.ts
+++ apps/web/src/app/billing/shared/payment-method.component.ts
@@ -142,9 +142,7 @@ export class PaymentMethodComponent implements OnInit {
     request.amount2 = this.verifyBankForm.value.amount2;
     await this.organizationApiService.verifyBank(this.organizationId, request);
     this.platformUtilsService.showToast("success", null, this.i18nService.t("verifiedBankAccount"));
-    // FIXME: Verify that this floating promise is intentional. If it is, add an explanatory comment and ensure there is proper error handling.
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    this.load();
+    await this.load();
   };
 
   submitTaxInfo = async () => {
