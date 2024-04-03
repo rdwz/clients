6546a2cdebd8ae5a3d73efde21c730d323bfeab9
Jared Snider <jsnider@bitwarden.com>
2024-04-03 18:10:13 -0400

    PM-5501 - (1) Update state definitions (2) convert KeyDefs to UserKeyDefs (2) Remove everBeenUnlocked as we won't need it

--- libs/common/src/services/vault-timeout/vault-timeout-settings.state.ts
+++ libs/common/src/services/vault-timeout/vault-timeout-settings.state.ts
@@ -1,33 +1,23 @@
 import { VaultTimeoutAction } from "../../enums/vault-timeout-action.enum";
-import {
-  KeyDefinition,
-  VAULT_TIMEOUT_SETTINGS_DISK_LOCAL,
-  VAULT_TIMEOUT_SETTINGS_MEMORY,
-} from "../../platform/state";
+import { UserKeyDefinition, VAULT_TIMEOUT_SETTINGS_DISK_LOCAL } from "../../platform/state";
 
 /**
  * Settings use disk storage and local storage on web so settings can persist after logout.
  */
-export const VAULT_TIMEOUT_ACTION = new KeyDefinition<VaultTimeoutAction>(
+export const VAULT_TIMEOUT_ACTION = new UserKeyDefinition<VaultTimeoutAction>(
   VAULT_TIMEOUT_SETTINGS_DISK_LOCAL,
   "vaultTimeoutAction",
   {
     deserializer: (vaultTimeoutAction) => vaultTimeoutAction,
+    clearOn: [], // persisted on logout
   },
 );
 
-export const VAULT_TIMEOUT = new KeyDefinition<number>(
+export const VAULT_TIMEOUT = new UserKeyDefinition<number>(
   VAULT_TIMEOUT_SETTINGS_DISK_LOCAL,
   "vaultTimeout",
   {
     deserializer: (vaultTimeout) => vaultTimeout,
-  },
-);
-
-export const EVER_BEEN_UNLOCKED = new KeyDefinition<boolean>(
-  VAULT_TIMEOUT_SETTINGS_MEMORY,
-  "everBeenUnlocked",
-  {
-    deserializer: (everBeenUnlocked) => everBeenUnlocked,
+    clearOn: [], // persisted on logout
   },
 );
