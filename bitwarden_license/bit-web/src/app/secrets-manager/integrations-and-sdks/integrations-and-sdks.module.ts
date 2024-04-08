84bb55a67ac7c4e37ebfd61c26dfbefe137db549
nick-livefront <nick@livefront.com>
2024-04-08 15:26:25 -0500

    Initial add of SDKs component

--- bitwarden_license/bit-web/src/app/secrets-manager/integrations-and-sdks/integrations-and-sdks.module.ts
+++ bitwarden_license/bit-web/src/app/secrets-manager/integrations-and-sdks/integrations-and-sdks.module.ts
@@ -5,10 +5,11 @@ import { SecretsManagerSharedModule } from "../shared/sm-shared.module";
 import { IntegrationsComponent } from "./integrations/integrations.component";
 import { IntegrationsRoutingModule } from "./integrations-and-sdks-routing.module";
 import { IntegrationsAndSDKsComponent } from "./integrations-and-sdks.component";
+import { SdksComponent } from "./sdks/sdks.component";
 
 @NgModule({
   imports: [SecretsManagerSharedModule, IntegrationsRoutingModule],
-  declarations: [IntegrationsAndSDKsComponent, IntegrationsComponent],
+  declarations: [IntegrationsAndSDKsComponent, IntegrationsComponent, SdksComponent],
   providers: [],
 })
 export class IntegrationsAndSDKsModule {}
