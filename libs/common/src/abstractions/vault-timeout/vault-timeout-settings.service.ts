3a66b047664717b75d7fb900e59dd779d82064ec
Jared Snider <jsnider@bitwarden.com>
2024-04-08 17:09:48 -0400

    PM-5501 - VaultTimeoutSettingsSvc - write up abstraction js docs

--- libs/common/src/abstractions/vault-timeout/vault-timeout-settings.service.ts
+++ libs/common/src/abstractions/vault-timeout/vault-timeout-settings.service.ts
@@ -2,15 +2,17 @@ import { Observable } from "rxjs";
 
 import { VaultTimeoutAction } from "../../enums/vault-timeout-action.enum";
 import { PinLockType } from "../../services/vault-timeout/vault-timeout-settings.service";
+import { UserId } from "../../types/guid";
 
 export abstract class VaultTimeoutSettingsService {
   /**
    * Set the vault timeout options for the user
    * @param vaultTimeout The vault timeout in minutes
    * @param vaultTimeoutAction The vault timeout action
-   * @param userId The user id to set. If not provided, the current user is used
+   * @param userId The user id to set the data for.
    */
   setVaultTimeoutOptions: (
+    userId: UserId,
     vaultTimeout: number,
     vaultTimeoutAction: VaultTimeoutAction,
   ) => Promise<void>;
@@ -24,19 +26,21 @@ export abstract class VaultTimeoutSettingsService {
   availableVaultTimeoutActions$: (userId?: string) => Observable<VaultTimeoutAction[]>;
 
   /**
-   * Get the current vault timeout action for the user. This is not the same as the current state, it is
-   * calculated based on the current state, the user's policy, and the user's available unlock methods.
+   * Gets the vault timeout action for the given user id. The returned value is
+   * calculated based on the current state, if a max vault timeout policy applies to the user,
+   * and what the user's available unlock methods are.
+   *
+   * A new action will be emitted if the current state changes or if the user's policy changes and the new policy affect the action.
+   * @param userId - the user id to get the vault timeout action for
    */
-  getVaultTimeout: (userId?: string) => Promise<number>;
+  getVaultTimeoutActionByUserId$: (userId: string) => Observable<VaultTimeoutAction>;
 
   /**
-   * Observe the vault timeout action for the user. This is calculated based on users preferred lock action saved in the state,
-   * the user's policy, and the user's available unlock methods.
-   *
-   * **NOTE:** This observable is not yet connected to the state service, so it will not update when the state changes
-   * @param userId The user id to check. If not provided, the current user is used
+   * Get the vault timeout for the given user id. The returned value is calculated based on the current state
+   * and if a max vault timeout policy applies to the user.
+   * @param userId The user id to get the vault timeout for
    */
-  vaultTimeoutAction$: (userId?: string) => Observable<VaultTimeoutAction>;
+  getVaultTimeoutByUserId$: (userId: string) => Observable<number>;
 
   /**
    * Has the user enabled unlock with Pin.
