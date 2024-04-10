6f9d6e73d2073a2189945d9a0f778ab2a561de3c
nick-livefront <nick@livefront.com>
2024-04-10 12:51:56 -0500

    remove integration & sdk components in favor of a single component

--- bitwarden_license/bit-web/src/app/secrets-manager/integrations-and-sdks/integrations/integrations.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "sm-integrations",
-  templateUrl: "./integrations.component.html",
-})
-export class IntegrationsComponent {}
--- bitwarden_license/bit-web/src/app/secrets-manager/integrations-and-sdks/sdks/sdks.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "sm-sdks",
-  templateUrl: "./sdks.component.html",
-})
-export class SdksComponent {}
